c26e780cb48d4258d720d5786d1a4e1fbee9f2bd
Rafael Gutiérrez <ragutimar@gmail.com>
2019-05-27 13:18:35 +0200

    Segregate JSON features to a separate package

--- src/globals.php
+++ src/globals.php
@@ -2,7 +2,9 @@
 
 /** @var string TRIGGER_ERROR_READ_ONLY_MODE Constant literal for Error message. */
 const TRIGGER_ERROR_READ_ONLY_MODE = "Instance is in Read Only mode and cannot be edited.";
+/** @var string TRIGGER_ERROR_INVALID_KEY Constant literal for Error message. */
 const TRIGGER_ERROR_INVALID_KEY = "Invalid key supplied [%s].";
+/** @var string TRIGGER_ERROR_INVALID_INDEX Constant literal for Error message. */
 const TRIGGER_ERROR_INVALID_INDEX = "Invalid index supplied [%s].";
+/** @var string TRIGGER_ERROR_INVALID_ARGUMENT Constant literal for Error message. */
 const TRIGGER_ERROR_INVALID_ARGUMENT = "Invalid argument [%s].";
-const TRIGGER_ERROR_INVALID_FILE = "Invalid file [%s].";
